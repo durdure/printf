ad551bd4ead3df188484c2e5db9909129dadfddd
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-03-26 10:03:03 +0300

    main.h

--- main.h
+++ main.h
@@ -3,17 +3,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <limits.h>
-#include <unsited.h>
+#include <unistd.h>
+
 
 
 /**
  * struct format - match the conversion specifiers for printf
  * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
  * @f: type pointer to function for the conversion specifier
+ *
  */
- 
- typedef struct format
+
+typedef struct format
 {
 	char *id;
 	int (*f)();
