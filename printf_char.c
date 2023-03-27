421c7a5fe2362868686346a1bbeac46977aa1b69
Duresa Eshetu <duresaeshetu2001@gmail.com>
2023-03-28 00:20:36 +0300

    print char

--- printf_char.c
+++ printf_char.c
@@ -1,15 +1,19 @@
-include "main.h"
+#include "main.h"
 
 /**
- * printf_char - prints a char.
- * @val: arguments.
- * Return: 1.
+ * print_char - Prints a char
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
  */
-int printf_char(va_list val)
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
-	char s;
+	char c = va_arg(types, int);
 
-	s = va_arg(val, int);
-	_putchar(s);
-	return (1);
+	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
